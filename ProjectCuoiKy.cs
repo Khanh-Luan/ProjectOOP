bc6955ae57491bc0ee79816e446059deb4725c63
Khanh-Luan <luankeng2007@gmail.com>
2026-09-21 03:10:50 +0000

    Add files via upload

---
+++ ProjectCuoiKy.cs
@@ -0,0 +1,72 @@
+using System;
+
+public abstract class MonAn
+{
+    // fields
+    protected string sMaMon;
+    protected string sTenMon;
+    protected double dGiaMon;
+    protected double dGiaBan;
+    // properties
+    public string MaMon
+    {
+        get { return this.sMaMon; }
+        set { this.sMaMon = value; }
+    }
+
+    public string TenMon
+    {
+        get { return this.sTenMon; }
+        set { this.sTenMon = value; }
+    }
+    public double GiaMon
+    {
+        get { return this.dGiaMon; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(value)}, the price must be greater than 0.");
+            }
+            this.dGiaMon = value;
+        }
+    }
+    // constructors
+    public MonAn()
+    {
+    }
+    public MonAn(string maMon, string tenMon, double giaMon)
+    {
+        this.MaMon = maMon;
+        this.TenMon = tenMon;
+        this.GiaMon = giaMon;
+    }
+    //destructors
+    ~MonAn() { }
+
+    // method
+    // Hàm nhập
+    public virtual void Nhap()
+    {
+        Console.WriteLine("Nhap ma cua mon an: ");
+        this.MaMon = Console.ReadLine();
+        Console.WriteLine("Nhap ten cua mon an: ");
+        this.TenMon = Console.ReadLine();
+        Console.WriteLine("Nhap gia ban cua mon an: ");
+        this.GiaMon = Convert.ToDouble(Console.ReadLine());
+    }
+    public virtual void Nhap(string maMon, string tenMon, double giaMon)
+    {
+        this.MaMon = maMon;
+        this.TenMon = tenMon;
+        this.GiaMon = giaMon;
+    }
+    public virtual void Xuat()
+    {
+        Console.WriteLine($"Ma cua mon an: {this.sMaMon}");
+        Console.WriteLine($"Ten cua mon an: {this.sTenMon}");
+        Console.WriteLine($"Gia ban cua mon an: {this.dGiaMon}");
+    }
+    public abstract void TinhGiaBan();
+}
+
